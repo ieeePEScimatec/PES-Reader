09590c4830ff8cad6f50fcab4cc613880559960b
RaulBernardes <107512218+RaulBernardes@users.noreply.github.com>
2023-05-23 21:34:26 -0300

    Add files via upload

---
+++ RFID_TESTEE.ino
@@ -0,0 +1,44 @@
+#include <Wire.h> //INCLUSÃO DA BIBLIOTECA NECESSÁRIA
+#include <SPI.h> //INCLUSÃO DE BIBLIOTECA
+#include <MFRC522.h> //INCLUSÃO DE BIBLIOTECA
+ 
+#define SS_PIN 10 //PINO SDA
+#define RST_PIN 9 //PINO DE RESET
+ 
+MFRC522 rfid(SS_PIN, RST_PIN); //PASSAGEM DE PARÂMETROS REFERENTE AOS PINOS
+ 
+ 
+const int pinoLedVerde = 3; //PINO DIGITAL REFERENTE AO LED VERDE
+const int pinoLedVermelho = 2; //PINO DIGITAL REFERENTE AO LED VERMELHO
+ 
+void setup(){
+ 
+  Wire.begin(); //INICIALIZA A BIBLIOTECA WIRE
+  SPI.begin(); //INICIALIZA O BARRAMENTO SPI
+  rfid.PCD_Init(); //INICIALIZA MFRC522
+  
+  pinMode(pinoLedVerde, OUTPUT); //DEFINE O PINO COMO SAÍDA
+  pinMode(pinoLedVermelho, OUTPUT); //DEFINE O PINO COMO SAÍDA
+  
+  digitalWrite(pinoLedVerde, LOW); //LED INICIA DESLIGADO
+  digitalWrite(pinoLedVermelho, LOW); //LED INICIA DESLIGADO
+}
+ 
+void loop() {
+  leituraRfid(); //CHAMA A FUNÇÃO RESPONSÁVEL PELA VALIDAÇÃO DA TAG RFID
+}
+ 
+//FUNÇÃO DE VALIDAÇÃO DA TAG RFID
+void leituraRfid(){
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) //VERIFICA SE O CARTÃO PRESENTE NO LEITOR É DIFERENTE DO ÚLTIMO CARTÃO LIDO. CASO NÃO SEJA, FAZ
+    return; //RETORNA PARA LER NOVAMENTE
+ 
+  /***INICIO BLOCO DE CÓDIGO RESPONSÁVEL POR GERAR A TAG RFID LIDA***/
+  String strID = ""; 
+  for (byte i = 0; i < 4; i++) {
+    strID +=
+    (rfid.uid.uidByte[i] < 0x10 ? "0" : "") +
+    String(rfid.uid.uidByte[i], HEX) +
+    (i!=3 ? ":" : "");
+  }
+  strID.toUpperCase();
